ed59d3db8ef7fd8b823a89c60cd0780251e9c48a
strawberryoolongtea <strawberrycream@kakao.com>
2022-03-16 01:46:34 +0900

    Using localstorage

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const h1 = document.querySelector("h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("h1");
 
-function handleTitleClick() {
-  // * replace to toggle
-  // const clickedClass = "active";
-  // if (h1.classList.contains(clickedClass)) {
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
-  // }
+const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-  // * toggle
-  h1.classList.toggle("active");
+const handleSubmit = (event) => {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+};
+
+const paintGreeting = (username) => {
+  greeting.innerText = `안녕하세요! ${username}님`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+if (savedUsername === null) {
+  // Show form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleSubmit);
+} else {
+  // Show greeting
+  paintGreeting(savedUsername);
 }
-
-h1.addEventListener("click", handleTitleClick);
